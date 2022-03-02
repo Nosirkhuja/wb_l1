ef8ea1e0e3d549932134b35365a3a7ba7846055d
Nosir Homidov <42383214+Nosirkhuja@users.noreply.github.com>
2022-03-02 17:40:50 +0300

    Update 9.go

--- 9.go
+++ 9.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Pow(chIn <-chan int, chOut chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for v := range chIn {
+		chOut <- v * v
+	}
+
+	close(chOut)
+}
+
+func Sender(chOut chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for _, v := range [10]int{31, 142, 23, 3, 12, 87, 32, 56, 23, 54} {
+		chOut <- v
+	}
+
+	close(chOut)
+}
+
+func Printer(chIn <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for v := range chIn {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	chIn := make(chan int)
+	chOut := make(chan int)
+
+	wg := new(sync.WaitGroup)
+
+	wg.Add(3)
+
+	go Sender(chIn, wg)
+	go Pow(chIn, chOut, wg)
+	go Printer(chOut, wg)
+
+	wg.Wait()
+}
